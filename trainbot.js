02713368df47d5d4e5ecd24666326ef2d61af3a4
waterafall <42365872+waterafall@users.noreply.github.com>
2018-08-14 11:03:46 +0700

    Add files via upload

---
+++ trainbot.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone : true});
+require('dotenv/config')
+bot.on('ready', async () => {
+	var channel = bot.channels.get(process.env.channel);
+      var interval = setInterval (function () {
+        channel.send("t!tg train");
+        channel.send("t!tg play");
+        channel.send("t!tg feed");
+        channel.send("t!tg clean");
+      }, 10 * 1200); 
+});
+bot.login(process.env.token);
